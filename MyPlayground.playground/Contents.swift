d81ad325ba498b151875b2b3b215553488aad8af
Aarsh Patil <patilaarsh@gmail.com>
2016-05-28 09:20:06 +0530

    Added all developed projects

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+func calculateSquareOfNumber(x: Int) -> Int {
+    return x * x
+}
+
+let result = calculateSquareOfNumber(2)
+
+print("\(result) is out ")
